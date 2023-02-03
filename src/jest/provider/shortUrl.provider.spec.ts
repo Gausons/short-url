fe63f89a64e533623a29d1dce506ee4f32488f71
gaocong <gaocong@authing.cn>
2023-02-03 13:15:41 +0800

    docs: update readme

--- src/jest/provider/shortUrl.provider.spec.ts
+++ src/jest/provider/shortUrl.provider.spec.ts
@@ -1,19 +1,23 @@
 import { ShortUrlProvider } from '../../provider/shortUrl.provider';
 import { DbConnection } from '../../common/db/dbConnectionOrm.db';
-
+import { random } from 'lodash';
+import { createShortUrl } from '../../common/utils/shortUrlCreate.utils'
 describe('ShortUrlProvider', () => {
   let shortUrlProvider: ShortUrlProvider;
   let dbConnection: DbConnection;
-
+  let shortUrl: string;
+  let longUrl: string;
   beforeAll(async () => {
     dbConnection = await DbConnection.getInstance();
     shortUrlProvider = new ShortUrlProvider();
+    shortUrl = await createShortUrl();
+    longUrl = `http://www.${Date.now()}.com`;
   });
 
   describe('saveShortUrl', () => {
     it('should save a new short url', async () => {
-      const shortUrl = 'ZSObExH6';
-      const longUrl = 'http://www.test6.com';
+      // const shortUrl = 'ZSObExH6';
+      // const longUrl = 'http://www.test6.com';
       const result = await shortUrlProvider.saveShortUrl(shortUrl, longUrl);
       expect(result).toEqual(shortUrl);
     });
@@ -21,8 +25,8 @@ describe('ShortUrlProvider', () => {
 
   describe('getShortUrlByLongUrl', () => {
     it('should get a short url by long url', async () => {
-      const shortUrl = 'ZSObExH6';
-      const longUrl = 'http://www.test6.com';
+      // const shortUrl = 'ZSObExH6';
+      // const longUrl = 'http://www.test6.com';
       const result = await shortUrlProvider.getShortUrlByLongUrl(longUrl);
       expect(result).toEqual(shortUrl);
     });
@@ -30,8 +34,8 @@ describe('ShortUrlProvider', () => {
 
   describe('getLongUrlByShortUrl', () => {
     it('should get a long url by short url', async () => {
-      const shortUrl = 'ZSObExH6';
-      const longUrl = 'http://www.test6.com';
+      // const shortUrl = 'ZSObExH6';
+      // const longUrl = 'http://www.test6.com';
       const result = await shortUrlProvider.getLongUrlByShortUrl(shortUrl);
       expect(result).toEqual(longUrl);
     });
